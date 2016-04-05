a252d095bd4b1d45e60d117426623192cc10cbe5
Laryssa Seabra <llaryssa@me.com>
2016-04-05 15:06:23 -0400

    Initial Commit

---
+++ CS600_P-8.2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS600_P-8.2
+//
+//  Created by Laryssa Seabra on 4/5/16.
+//  Copyright (c) 2016 Alana Laryssa S A Santos. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
